2f863ce10b7cdf7123d53d28452b6f960e710f93
420ma <420ma@172.20.39.43>
2018-03-14 09:40:31 +0100

    Creazione Interfaccia

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -1,14 +1,11 @@
-package it.polito.tdp.alien;
-
 /**
  * Sample Skeleton for 'Alien.fxml' Controller Class
  */
 
-
+package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,40 +13,42 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
-	
-    @FXML
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
-    @FXML
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
-    @FXML
-    private TextField txtWord;
-    @FXML
-    private TextArea txtResult;
-    @FXML
-    private Button btnTranslate;
-    @FXML
-    private Button btnReset;
-        
-    
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-    	assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
-    	
-    }
-  
-    
-    @FXML
-    void doTranslate(ActionEvent event) {
-    	    	
-    }
-    
-    
+
+    @FXML // fx:id="txtWord"
+    private TextField txtWord; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnTranslate"
+    private Button btnTranslate; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
     @FXML
     void doReset(ActionEvent event) {
 
     }
-    
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
+
+    }
 }
+
